aea841148f817174385bb4d70ba333c8be68bb7c
Napster <napster@npstr.space>
2018-05-11 13:42:15 +0200

    Create HttpClientConfiguration

---
+++ src/main/java/space/npstr/wolfia/config/HttpClientConfiguration.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2017-2018 Dennis Neufeld
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package space.npstr.wolfia.config;
+
+import okhttp3.OkHttpClient;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by napster on 11.05.18.
+ */
+@Configuration
+public class HttpClientConfiguration {
+
+    //a general purpose http client builder
+    @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE) //do not reuse the builders
+    public static OkHttpClient.Builder httpClientBuilder() {
+        return new OkHttpClient.Builder()
+                .connectTimeout(30, TimeUnit.SECONDS)
+                .writeTimeout(30, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .retryOnConnectionFailure(true);
+    }
+
+    // default http client that can be used for anything
+    @Bean
+    public OkHttpClient defaultHttpClient(final OkHttpClient.Builder httpClientBuilder) {
+        return httpClientBuilder
+//                .eventListener(new Metrics.HttpEventListener("default")) todo add metrics
+                .build();
+    }
+}
